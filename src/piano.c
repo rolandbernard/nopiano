35e48c3d28d22b14b604a4741be264b79799cf45
Roland Bernard <rolli.bernard@gmail.com>
2020-03-23 20:16:36 +0100

    Mapping enter to 52

--- src/piano.c
+++ src/piano.c
@@ -189,7 +189,7 @@ const int keys[] = {
     67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 95, 96,
     10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
-    38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 36,
+    38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 51,
     94, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62,
 };
 
@@ -197,9 +197,9 @@ void initKeys(Display* display) {
     Window root_window = DefaultRootWindow(display);
 
     for (int i = 0; i < sizeof(keys)/sizeof(keys[0]); i++) {
-        XGrabKey(display, keys[i], 0, root_window, false, GrabModeAsync, GrabModeAsync);
         keycode_to_note[keys[i]] = i;
     }
+    keycode_to_note[XKeysymToKeycode(display, XK_Return)] = keycode_to_note[51];
     XGrabKeyboard(display, root_window, false, GrabModeAsync, GrabModeAsync, CurrentTime);
 
     XAutoRepeatOff(display);
